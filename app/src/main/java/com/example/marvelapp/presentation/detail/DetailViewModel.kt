d2a0cd30a721bb62b39f9917da57024d9dc6d388
gustavo.p.rocha <pereiragustavo215@gmail.com>
2022-09-22 10:12:20 -0300

    implementando useCase no DetailViewModel.kt

--- app/src/main/java/com/example/marvelapp/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/marvelapp/presentation/detail/DetailViewModel.kt
@@ -1,5 +1,44 @@
 package com.example.marvelapp.presentation.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.core.domain.model.Comic
+import com.example.core.usecase.GetComicsUseCase
+import com.example.core.usecase.base.ResultStatus
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DetailViewModel : ViewModel()
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val getComicsUseCase: GetComicsUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableLiveData<UiState>()
+    val uiState: LiveData<UiState> get() = _uiState
+
+    fun geComics(characterId: Int) = viewModelScope.launch {
+        getComicsUseCase(GetComicsUseCase.GetComicsParams(characterId)).watchStatus()
+    }
+
+    private fun Flow<ResultStatus<List<Comic>>>.watchStatus() = viewModelScope.launch {
+        collect { status ->
+            when (status) {
+                // Sempre que for uma data Class deve chamar o is. Já para object não é necessário.
+                // Isso pq o object não tem tipo e valor interno.
+                ResultStatus.Loading -> UiState.Loading
+                is ResultStatus.Success -> UiState.Success(status.data)
+                is ResultStatus.Error -> UiState.Error
+            }
+        }
+    }
+
+    sealed class UiState {
+        object Loading : UiState()
+        data class Success(val comics: List<Comic>) : UiState()
+        object Error : UiState()
+    }
+}
