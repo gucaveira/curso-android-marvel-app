85feaa642daa868d16605ffe5bfe0f139655f782
gustavo rocha <pereiragustavo215@gmail.com>
2022-09-26 14:32:42 -0300

    implementando o caso de uso para adicionar um favorito

--- app/src/main/java/com/example/marvelapp/framework/di/UseCaseModule.kt
+++ app/src/main/java/com/example/marvelapp/framework/di/UseCaseModule.kt
@@ -1,9 +1,11 @@
 package com.example.marvelapp.framework.di
 
+import com.example.core.usecase.AddFavoriteUseCase
+import com.example.core.usecase.AddFavoriteUseCaseImpl
+import com.example.core.usecase.GetCharacterCategoriesUseCase
+import com.example.core.usecase.GetCharacterCategoriesUseCaseImpl
 import com.example.core.usecase.GetCharactersUseCase
 import com.example.core.usecase.GetCharactersUseCaseImpl
-import com.example.core.usecase.GetCharacterCategoriesUseCaseImpl
-import com.example.core.usecase.GetCharacterCategoriesUseCase
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -18,4 +20,7 @@ interface UseCaseModule {
 
     @Binds
     fun bindGetComicsUseCase(useCase: GetCharacterCategoriesUseCaseImpl): GetCharacterCategoriesUseCase
+
+    @Binds
+    fun bindAddFavoriteUseCase(useCase: AddFavoriteUseCaseImpl): AddFavoriteUseCase
 }
